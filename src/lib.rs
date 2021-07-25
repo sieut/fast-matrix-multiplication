37222f1920b8048ae653a99fbdf34d34b1b6455c
Sieu Tsoi <sieut@umich.edu>
2021-07-25 13:11:02 -0700

    SIMD Baby
    So fast, so so fast, 2x tdata_cached even

--- src/lib.rs
+++ src/lib.rs
@@ -72,12 +72,11 @@ impl Matrix {
             let row = a.row(i);
             for j in (0..mat.dim.1).step_by(INTS_PER_LINE) {
                 let cols = b.cols_iter(j);
-                let per_iter = cols.per_iter;
                 row.iter().zip(cols)
                     .for_each(|(row_ele, cols_ele)|
-                        Matrix::row_cols_mul(row_ele, cols_ele, &mut buffer));
-                for k in j..j + per_iter {
-                    mat.set(i, k, buffer[k - j]);
+                        Matrix::vec_scalar_mul(row_ele, cols_ele, &mut buffer));
+                for k in j..j + INTS_PER_LINE {
+                    mat.data[i * mat.row_size + k] = buffer[k - j];
                 }
                 buffer = [0i32; INTS_PER_LINE];
             }
@@ -85,10 +84,53 @@ impl Matrix {
         mat
     }
 
-    fn row_cols_mul(row_ele: &i32, cols_ele: &[i32], buffer: &mut [i32]) {
-        // TODO SIMD
-        for (i, col_ele) in cols_ele.iter().enumerate() {
-            buffer[i] += row_ele * col_ele;
+    /// Perform scalar multiplication on vec, then adds the result to output
+    /// using SIMD instructions
+    #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"),
+                  target_feature = "avx2"))]
+    fn vec_scalar_mul(
+        scalar: &i32,
+        vec: &[i32],
+        output: &mut [i32],
+    ) {
+        unsafe {
+            #[cfg(target_arch = "x86")]
+            use std::arch::x86::{
+                _mm256_set_epi32, _mm256_load_si256, _mm256_mullo_epi32,
+                _mm256_add_epi32, __m256i};
+            #[cfg(target_arch = "x86_64")]
+            use std::arch::x86_64::{
+                _mm256_set_epi32, _mm256_load_si256, _mm256_mullo_epi32,
+                _mm256_add_epi32, __m256i};
+            let scalar = _mm256_set_epi32(
+                scalar.clone(), scalar.clone(), scalar.clone(), scalar.clone(),
+                scalar.clone(), scalar.clone(), scalar.clone(), scalar.clone());
+            // Multiplication and addition on first 8 ints
+            let simd_vec = _mm256_load_si256(vec.as_ptr() as *const __m256i);
+            let simd_vec = _mm256_mullo_epi32(scalar, simd_vec);
+            let buffer = _mm256_load_si256(output.as_ptr() as *const __m256i);
+            let buffer = _mm256_add_epi32(simd_vec, buffer);
+            output[0..8].copy_from_slice(
+                &std::mem::transmute::<__m256i, [i32; 8]>(buffer));
+            // Multiplication and addition on second 8 ints
+            let simd_vec = _mm256_load_si256(vec.as_ptr().add(8) as *const __m256i);
+            let simd_vec = _mm256_mullo_epi32(scalar, simd_vec);
+            let buffer = _mm256_load_si256(output.as_ptr().add(8) as *const __m256i);
+            let buffer = _mm256_add_epi32(simd_vec, buffer);
+            output[8..INTS_PER_LINE].copy_from_slice(
+                &std::mem::transmute::<__m256i, [i32; 8]>(buffer));
+        }
+    }
+
+    /// Perform scalar multiplication on vec, then adds the result to output
+    #[cfg(not(target_feature = "avx2"))]
+    fn vec_scalar_mul(
+        scalar: &i32,
+        vec: &[i32],
+        output: &mut [i32],
+    ) {
+        for (i, col_ele) in vec.iter().enumerate() {
+            output[i] += scalar * col_ele;
         }
     }
 
@@ -144,17 +186,17 @@ impl Matrix {
         let mut mat = Matrix::new(dim);
         for i in 0..mat.dim.0 {
             for j in 0..mat.dim.1 {
-                mat.set(i, j, rand::random::<i16>() as i32);
+                mat.set(i, j, rand::random::<i8>() as i32);
             }
         }
         mat
     }
 }
 
+/// Iterate through elements of INTS_PER_LINE columns at once
 struct ColsIter<'a> {
     mat: &'a Matrix,
     col_idx: usize,
-    per_iter: usize,
     idx: usize,
 }
 
@@ -163,7 +205,6 @@ impl<'a> ColsIter<'a> {
         ColsIter {
             mat,
             col_idx,
-            per_iter: std::cmp::min(INTS_PER_LINE, mat.dim.1 - col_idx),
             idx: 0,
         }
     }
@@ -178,7 +219,30 @@ impl<'a> std::iter::Iterator for ColsIter<'a> {
         } else {
             let data_idx = self.idx * self.mat.row_size + self.col_idx;
             self.idx += 1;
-            Some(&self.mat.data[data_idx..data_idx + self.per_iter])
+            Some(&self.mat.data[data_idx..data_idx + INTS_PER_LINE])
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiplications() {
+        let a = Matrix::rand_matrix((24, 24));
+        let b = Matrix::rand_matrix((24, 24));
+        let naive = Matrix::naive_mul(&a, &b);
+        assert_eq!(naive.data, Matrix::cached_tdata_mul(&a, &b).data);
+        assert_eq!(naive.data, Matrix::cacheline_optimized_col_mul(&a, &b).data);
+        assert_eq!(naive.data, Matrix::preload_slice_mul(&a, &b).data);
+    }
+
+    #[test]
+    fn test_vec_scalar_mul() {
+        let vec = [2i32; INTS_PER_LINE];
+        let mut buffer  = [0i32; INTS_PER_LINE];
+        Matrix::vec_scalar_mul(&5, &vec[..], &mut buffer);
+        assert_eq!([10; INTS_PER_LINE], buffer);
     }
 }
