ca0f04acd025f6c45faaa2590a24a3d779671d1e
Sieu Tsoi <sieut@umich.edu>
2021-07-25 13:23:42 -0700

    Better benchmark setup

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,37 +5,24 @@ extern crate rand;
 use criterion::{criterion_group, criterion_main, Criterion, black_box};
 use fmm::Matrix;
 
-pub fn naive_benchmark(c: &mut Criterion) {
+fn bench_muls(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Matrix Multiplication");
     let a = Matrix::rand_matrix((100, 120));
     let b = Matrix::rand_matrix((120, 200));
-    c.bench_function(
+
+    group.bench_function(
         "Naive Multiplication",
         |bencher| bencher.iter(|| Matrix::naive_mul(black_box(&a), black_box(&b))));
-}
-
-pub fn cached_tdata_benchmark(c: &mut Criterion) {
-    let a = Matrix::rand_matrix((100, 120));
-    let b = Matrix::rand_matrix((120, 200));
-    c.bench_function(
+    group.bench_function(
         "Cached Matrix Transpose Multiplication",
         |bencher| bencher.iter(|| Matrix::cached_tdata_mul(black_box(&a), black_box(&b))));
-}
-
-pub fn cacheline_optimized_col_benchmark(c :&mut Criterion) {
-    let a = Matrix::rand_matrix((100, 120));
-    let b = Matrix::rand_matrix((120, 200));
-    c.bench_function(
+    group.bench_function(
         "Cacheline Optimized Columns Multiplication",
         |bencher| bencher.iter(|| Matrix::cacheline_optimized_col_mul(black_box(&a), black_box(&b))));
-}
-
-pub fn preload_slice_benchmark(c: &mut Criterion) {
-    let a = Matrix::rand_matrix((100, 120));
-    let b = Matrix::rand_matrix((120, 200));
-    c.bench_function(
+    group.bench_function(
         "Preload Slices Matrix Multiplication",
         |bencher| bencher.iter(|| Matrix::preload_slice_mul(black_box(&a), black_box(&b))));
 }
 
-criterion_group!(benches, naive_benchmark, cached_tdata_benchmark, cacheline_optimized_col_benchmark, preload_slice_benchmark);
+criterion_group!(benches, bench_muls);
 criterion_main!(benches);
